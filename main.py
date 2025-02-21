Bug fixes in noise.py and main.py for computing averaged blank frames

main.py
@@ -19,12 +19,12 @@ import psutil
 
 # Create JIT-compiled version for JAX operations
 bin_xy_jit = jax.jit(bin_xy, static_argnames=['binsize'])
-# subtract_bg_jit = jax.jit(subtract_bg, static_argnames=['bitdepth'])
 bin_and_subtract_jit = jax.jit(bin_and_subtract, static_argnames=['binsize', 'bitdepth'])
 
-def print_mem_usage(label):
+def print_mem_usage(gpu):
     process = psutil.Process(os.getpid())
-    print(f"[{label}] Memory usage: {process.memory_info().rss / 1024 ** 2:.2f} MB")
+    print(f"Process Memory usage: {process.memory_info().rss / 1024 ** 2:.2f} MB")
+    print(f"GPU memory stats: {gpu.memory_stats()}")
 
 def preprocess(
     input_path, output_dir, 
@@ -36,18 +36,19 @@ def preprocess(
     save_as,
     spacing
 ):
+    # Get the first GPU device (if available)
+    gpu = jax.devices("gpu")[0]
 
     # Load noise data if a .tif file is provided, or compute noise by averaging frames in blank_dir
     noise_data = None
+
+    if blank_dir:
+        noise_path = get_noise_data(blank_dir)
+        print(f"Averaged blank frames from {blank_dir} is saved under {noise_path}.")
     
     if noise_path:
         noise_data = jnp.array(tifffile.imread(str(noise_path)))
-        noise_data = bin_xy_jit(noise_data, binsize) # bin to superpixel
         print(f"Loaded noise data of shape {noise_data.shape} from {noise_path}")
-    elif blank_dir:
-        noise_data, savepath = get_noise_data(blank_dir)
-        noise_data = bin_xy_jit(noise_data, binsize) # bin to superpixel
-        print(f"Computed noise data of shape {noise_data.shape} by averaging frames from {blank_dir}.")
     else:
         print("⚠️⚠️⚠️ WARNING: No blank frames provided. Assuming uniform background for all pixels.⚠️⚠️⚠️")
 
@@ -58,7 +59,6 @@ def preprocess(
         prefix = re.findall(r'202\d{1}-\d{2}-\d{2}-\d{2}', str(output_dir))
         prefix = prefix[-1] if prefix else ''
         base_nrrd_path = Path(output_dir) / "NRRD"
-        # base_mip_path = Path(output_dir) / "MIP"
     else: # .tif or .tiff
         base_tif_path = Path(output_dir)
         
@@ -77,38 +77,36 @@ def preprocess(
         all_frames = all_frames[:, :, z_range, :, :] # ignore the z-slices with lots of motion artifacts
     
     n_chunks = (t_size + chunk_size - 1) // chunk_size
-    dask.config.set(**{'array.slicing.split_large_chunks': True})
 
-    # Perform jax array computations in series, and file saving in parallel
+    # Perform jax array computations in series, and file saving in parallel   
     for chunk_idx in tqdm(range(n_chunks)):
         t_start = chunk_idx * chunk_size
         t_end = min(t_start + chunk_size, t_size)
         current_chunk_size = t_end - t_start
 
         # Bring a small chunk of the Dask array to RAM
-        temp = all_frames[t_start:t_end].compute()
-        chunk_data = jnp.array(temp)
-        del temp
-        # print_mem_usage("After chunk from dask to mem")
+        input_data = jax.device_put(jnp.array(all_frames[t_start:t_end].compute(), dtype=jnp.uint16))
 
         # If blank frames are not passed in, a uniform background assumed for subtraction
         if noise_data is None:
-            noise_data = compute_uniform_noise_data(chunk_data, bg_percentile) # compute noise per channel from the first chunk
+            noise_data = compute_uniform_noise_data(input_data, bg_percentile) # compute noise per channel from the first chunk
+            print(f"Uniform background of {noise_data[:,0,0]} will be subtracted from binned superpixels in the respective channels.")
+
+        # Load binned noise to GPU
+        if chunk_idx == 0:
             noise_data = bin_xy_jit(noise_data, binsize) # bin noise data to superpixels
             noise_data = jnp.floor(noise_data / 100) * 100  # Round down to nearest hundred
-            print(f"Uniform background of {noise_data[:,0,0]} will be subtracted from binned superpixels in the respective channels.")
+            noise_data = jax.device_put(noise_data)
         
         # Apply transformations
-        chunk_data = bin_and_subtract_jit(chunk_data, noise_data, binsize, bitdepth) # Bin to superpixel
-        chunk_data.block_until_ready()
+        output_data = bin_and_subtract_jit(input_data, noise_data, binsize, bitdepth).astype(jnp.uint16) # Bin to superpixel
             
         # Parallel NRRD saving
         if save_as == 'nrrd':
             tasks = [
                 (
                     base_nrrd_path / f"{prefix}_t{t_start + t_offset + 1:04d}_ch{c+1}.nrrd",
-                    # base_mip_path / f"{prefix}_t{t_start + t_offset + 1:04d}_ch{c+1}.png",
-                    chunk_data[t_offset, c, ...],  # Extract per timepoint per channel
+                    jax.device_get(output_data[t_offset, c]),  # Extract per timepoint per channel
                     spacing
                 )
                 for t_offset in range(current_chunk_size)
@@ -121,7 +119,7 @@ def preprocess(
             tasks = [
                 (
                     base_tif_path / f"{prefix}_t{t_start + 1:04d}_to_t{t_offset + 1:04d}_ch{c+1}.tif",
-                    chunk_data[:, c, ...]  # Extract per-channel time series
+                    jax.device_get(output_data[:, c])  # Extract per-channel time series
                 )
                 for c in range(n_channels)
             ]            
@@ -131,8 +129,9 @@ def preprocess(
             
         print(f"Finished processing chunk {chunk_idx+1}/{n_chunks}")
         gc.collect()
-        print_mem_usage("After garbage collection")
-
+        # jax.numpy.zeros(1).block_until_ready()  # Helps sync and free memory
+        if chunk_idx % 10 == 0 and chunk_idx > 0:
+            print_mem_usage(gpu)
         
 def main():
     parser = argparse.ArgumentParser(description='Process microscopy image data with background subtraction and binning.')

noise.py
@@ -4,54 +4,41 @@ from pathlib import Path
 import tifffile
 import os
 from tqdm import tqdm
+import nd2
 
-def get_noise_data_nd2(blank_files: [str], savepath: str, chunk_size: int):
+def get_noise_data_nd2(blank_files: [str], savepath: str):
     """JAX-accelerated computation of averaged noise array from multiple .nd2 files"""
     # Use the first file in the blank directory to initialize the noise array
     filepath = blank_files[0]
     with nd2.ND2File(filepath) as nd2_file:
         all_frames = nd2_file.to_dask() # [T*Z, C, X, Y]
-
-    n_x = all_frames.shape[-2]
-    n_y = all_frames.shape[-1]
-
-    if len(all_frames.shape) == 3: # single channel recording
-        all_frames.reshape(all_frames.shape[0], 1, *all_frames.shape[1:])
-        n_c = 1
-    elif len(all_frames.shape) == 4:
-        n_c = all_frames.shape[1]
+    n_pages, n_channels, n_x, n_y = all_frames.shape
 
     # Initialize separate noise data arrays for each channel
-    total_frames = 0
-    noise_data = jnp.mean(all_frames[0:1].compute(), axis=0) # [C, X, Y]
+    noise_data = None
     
     # Process files with progress bar
     for filepath in tqdm(blank_files, desc="Processing noise files"):
         with nd2.ND2File(filepath) as nd2_file:
             all_frames = nd2_file.to_dask() # [T*Z, C, X, Y]
         assert all_frames.shape[-2] == n_x and all_frames.shape[-1] == n_y, "All blank frames should have the same XY dimension!!!"
-
-        n_pages = all_frames.shape[0]
-        n_chunks = (n_pages + chunk_size - 1) // chunk_size
         
-        for chunk_idx in tqdm(range(n_chunks)):
-            # Bring a small chunk of the Dask array to RAM
-            t_start = chunk_idx * chunk_size
-            t_end = min(t_start + chunk_size, t_size)
-            current_chunk_size = t_end - t_start
-
-            chunk_data = jnp.array(all_frames[t_start:t_end].compute(), dtype=jnp.int16)
-            chunk_avg = jnp.mean(chunk_data, axis=0) # [C, X, Y]
+        n_pages, n_channels, n_x, n_y = all_frames.shape
+        to_ram = min(n_pages, 2000) # take the first 2000 frames if the blank files are longer than that
+        chunk_data = jnp.array(all_frames[:to_ram].compute())
+        chunk_avg = jnp.mean(chunk_data, axis=0) # [C, X, Y]
+        if noise_data is None:
+            noise_data = chunk_avg
+        else:
             noise_data = (noise_data + chunk_avg) /2
-            
-            total_frames += current_chunk_size
 
     noise_data = jnp.rint(noise_data).astype('int16')  # round to the nearest integer
+    print(f"Averaged noise is of dimension {noise_data.shape}")
     tifffile.imwrite(savepath, noise_data) # export tif file for next time
-    return noise_data
+    return savepath
 
 
-def get_noise_data(blank_dir: str, chunk_size: int = 160):
+def get_noise_data(blank_dir: str):
     """
     Compute average noise from blank images
 
@@ -67,7 +54,7 @@ def get_noise_data(blank_dir: str, chunk_size: int = 160):
     if not blank_files:
         raise ValueError(f"No .nd2 files found in directory: {blank_dir}")
         
-    return get_noise_data_nd2(blank_files, savepath, chunk_size)
+    return get_noise_data_nd2(blank_files, savepath)
 
 
 def compute_uniform_noise_data(chunk_data, bg_percentile):
@@ -93,4 +80,4 @@ def compute_uniform_noise_data(chunk_data, bg_percentile):
     noise_values = jax.vmap(compute_uniform_bg)(jnp.arange(n_channels))  # Shape: (C,)
     noise_data = jnp.broadcast_to(noise_values[:, None, None], (n_channels, n_x, n_y))
 
-    return noise_data # [C, X, Y]
+    return noise_data.astype(jnp.int16) # [C, X, Y]

process.py
@@ -7,7 +7,6 @@ def bin_xy(data, binsize):
     Parameters:
     - data: jnp.array of shape (..., X, Y) → Input image
     - binsize: int → Factor by which to downsample (bin) the image
-    - bitdepth: int (8, 11, 12, 16) → Bit depth for clipping
 
     Returns:
     - Binned and transposed array of shape (..., X//binsize, Y//binsize)
@@ -21,14 +20,14 @@ def bin_xy(data, binsize):
 
     # Sum over bins
     data_binned = data_binned.sum(axis=(-3, -1))  # Summing in XY only
+    print(f"Binned averaged noise is of dimension {data_binned.shape}")
     return data_binned
     
 
 def bin_and_subtract(chunk_data, noise_data, binsize, bitdepth):
 
-    # Sum over bins
-    chunk_data_binned = bin_xy(chunk_data, binsize)
-
+    chunk_data = bin_xy(chunk_data, binsize) # bin first
+    
     """
     Subtract background jnp array from real data jnp array, where (C, X, Y) dimensions are matching in size
 
@@ -40,9 +39,9 @@ def bin_and_subtract(chunk_data, noise_data, binsize, bitdepth):
     - chunk data: jnp.array of shape  (T, Z, C, X, Y)
     """
 
-    assert chunk_data_binned.shape[-2:] == noise_data.shape[-2:], "Real data and noise data should have identical XY dimensions"   
+    assert chunk_data.shape[-2:] == noise_data.shape[-2:], f"Real data {chunk_data.shape[-2:]} and noise data {noise_data.shape[-2:]} have different XY dimensions"   
     noise_expanded = jnp.expand_dims(noise_data, axis=(0, 2)) # (C, X, Y) -> (1, C, 1, X, Y)
-    noise_subtracted = chunk_data_binned - noise_expanded # matched to (T, C, Z, X, Y)
+    noise_subtracted = chunk_data - noise_expanded # matched to (T, C, Z, X, Y)
     clipped = jnp.clip(noise_subtracted, 0, 2 ** bitdepth) # Clip values to fit in bit range
     
     return jnp.transpose(clipped, axes=(0,1,4,3,2)) # Rearrange axes